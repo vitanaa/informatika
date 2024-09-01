38a0bcc3f374baa8825204be975e075e6b907a20
vitanaa <145916312+vitanaa@users.noreply.github.com>
2024-09-01 12:13:52 +0700

    Add files via upload

---
+++ implementasi informatika 1.py
@@ -0,0 +1,15 @@
+nama = "Musfita ainin nafisah"
+nama_panggilan = "vita"
+umur = 17
+tinggi = 163
+is_student = True
+warna = ["grey", "blue"]
+data = {"name": nama_panggilan, "age": umur}
+
+print("Name:", nama)
+print("Nickname:", nama_panggilan)
+print("Age:", umur)
+print("Height:", tinggi)
+print("Santri:", is_student)
+print("Favorite color:", warna)
+print("Personal data:", data)
